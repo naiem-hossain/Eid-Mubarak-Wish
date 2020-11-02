d26f67ad8ff359e0019ca3ac6d7c6c2b0e31c76f
Md. Naiem Hossain <73810796+naiem-hossain@users.noreply.github.com>
2020-11-02 18:28:00 +0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+var a=prompt("Enter User Name");
+ var b=prompt("Enter Password");
+  	if(a== "eid" && b=="mubarak")
+  	{
+  	    window.location.href="start.html";
+  	}
+  	else
+  	{
+  		alert("Sorry Your User Name & Password Invalid");
+  		window.location.href="index.html";
+  	}
+  	
